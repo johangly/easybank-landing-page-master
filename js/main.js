3dfba26515dca78a9c9330b11073be01d1670875
Johangly <johanglyfabiansucre@gmail.com>
2021-11-09 22:35:24 -0400

    v1.3 Frontend 100% Complete

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("body-id").classList.add("body-block")
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("body-id").classList.remove("body-block");
 }
